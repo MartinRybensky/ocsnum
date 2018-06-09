4045214b609dc240fe5699497deb71500479247c
Martin Rybensky <martin@rybensky.com>
2018-06-09 14:30:35 +0200

    initial commit

---
+++ ocsnum.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+# license Creative Commons BY-NC-SA 3.0
+# Created by Martin Rybensky
+
+__version__ = '0.01.01'
+__author__ = 'Martin Rybensky'
+
+import sys
+
+
+
+
+
+
+
+
+
+try:
+    user_input = sys.argv[1]
+except:
+    print 'you must specify a number as a parameter'
+    sys.exit(0)
+
+
+
+if user_input == '1596':
+
+	print 'тꙑсѫщи • пѧть сътъ • девѧть десѧтъ и шесть'
+	sys.exit(0)
+else:
+	print 'tohle cislo neznam'
